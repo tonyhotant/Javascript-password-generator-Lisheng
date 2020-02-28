852f092445c998bd3ae9444c6c39ef1bd4d1ba3b
Lisheng <lishengyu1986@gmail.com>
2020-02-29 00:28:13 +0800

    typo in js file

--- Develop/script.js
+++ Develop/script.js
@@ -51,6 +51,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
